24b97b2c628fd33eacc7e52f4502644cb4049649
Göksu Ayaz <goksuayaz79@gmail.com>
2024-12-29 17:44:52 +0300

    Sign In Screen

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,22 @@
+import {View, Text} from "react-native";
+import { Link } from "expo-router";
+
+export default function Index(){
+  return(
+  <View
+  style={{
+    flex:1,
+    justifyContent:"center",
+    alignItems:"center",
+
+  }}
+>
+<Text className="font-bold my-10 font-rubik text-3xl">Welcome to Restate </Text>
+<Link href="/sign-in"> Sign In </Link>
+<Link href="/explore"> Explore </Link>
+<Link href="/profile"> Profile </Link>
+<Link href="/properties/1"> Property </Link>
+
+</View>
+  )
+}
---
+++ app/(root)/_layout.tsx
@@ -0,0 +1,10 @@
+import { Redirect, Slot } from "expo-router";
+import { ActivityIndicator } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+
+
+export default function AppLayout() {
+
+  return <Slot />;
+}
